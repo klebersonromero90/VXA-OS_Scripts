923d963c2d6fc84e4c97244e3cd1460167d5e5b4
klebersonromero90 <35940620+klebersonromero90@users.noreply.github.com>
2018-05-13 23:32:49 -0300

    Update Fullscreen++ v2.2_VXA-OS.rb

--- Fullscreen++ v2.2_VXA-OS.rb
+++ Fullscreen++ v2.2_VXA-OS.rb
@@ -6,38 +6,58 @@
 # (fr) Manuel d'utilisation : http://pastebin.com/raw.php?i=1TQfMnVJ
 # (en) User Guide           : http://pastebin.com/raw.php?i=EgnWt9ur
 #-------------------------------------------------------------------------
-# Modificado por: Komuro Takashi
+# Modificado por: Komuro Takashi para o Projeto VXA-OS
 #-------------------------------------------------------------------------
 #=========================================================================== 
 $imported ||= {}
 $imported[:Zeus_Fullscreen] = __FILE__
-
 class << Graphics
-  Disable_VX_Fullscreen = false
- 
-  CreateWindowEx            = Win32API.new('user32'  , 'CreateWindowEx'           , 'ippiiiiiiiii', 'i')
-  GetClientRect             = Win32API.new('user32'  , 'GetClientRect'            , 'ip'          , 'i')
-  GetDC                     = Win32API.new('user32'  , 'GetDC'                    , 'i'           , 'i')
-  GetSystemMetrics          = Win32API.new('user32'  , 'GetSystemMetrics'         , 'i'           , 'i')
-  GetWindowRect             = Win32API.new('user32'  , 'GetWindowRect'            , 'ip'          , 'i')
-  FillRect                  = Win32API.new('user32'  , 'FillRect'                 , 'ipi'         , 'i')
-  FindWindow                = Win32API.new('user32'  , 'FindWindow'               , 'pp'          , 'i')
-  ReleaseDC                 = Win32API.new('user32'  , 'ReleaseDC'                , 'ii'          , 'i')
-  SendInput                 = Win32API.new('user32'  , 'SendInput'                , 'ipi'         , 'i')
-  SetWindowLong             = Win32API.new('user32'  , 'SetWindowLong'            , 'iii'         , 'i')
-  SetWindowPos              = Win32API.new('user32'  , 'SetWindowPos'             , 'iiiiiii'     , 'i')
-  ShowWindow                = Win32API.new('user32'  , 'ShowWindow'               , 'ii'          , 'i')
-  SystemParametersInfo      = Win32API.new('user32'  , 'SystemParametersInfo'     , 'iipi'        , 'i')
-  UpdateWindow              = Win32API.new('user32'  , 'UpdateWindow'             , 'i'           , 'i')
-  GetPrivateProfileString   = Win32API.new('kernel32', 'GetPrivateProfileString'  , 'ppppip'      , 'i')
-  WritePrivateProfileString = Win32API.new('kernel32', 'WritePrivateProfileString', 'pppp'        , 'i')
-  CreateSolidBrush          = Win32API.new('gdi32'   , 'CreateSolidBrush'         , 'i'           , 'i')
-  DeleteObject              = Win32API.new('gdi32'   , 'DeleteObject'             , 'i'           , 'i')
- 
+	#---------------------------------------------#
+  Gdi32    = DL.dlopen('gdi32')
+	User32   = DL.dlopen('user32')
+	Kernel32 = DL.dlopen('kernel32')
+	#---------------------------------------------#
+	# KOMURO WIN32API to DL
+	#---------------------------------------------#
+  	# USER32
+	#---------------------------------------------#
+	UpdateWindow              = DL::CFunc.new(  User32['UpdateWindow'        ], DL::TYPE_LONG)
+	FindWindow		  = DL::CFunc.new(  User32['FindWindow'          ], DL::TYPE_LONG)
+	CreateWindowEx		  = DL::CFunc.new(  User32['CreateWindowEx'      ], DL::TYPE_LONG)
+	GetSystemMetrics          = DL::CFunc.new(  User32['GetSystemMetrics'    ], DL::TYPE_LONG)
+	ShowWindow		  = DL::CFunc.new(  User32['ShowWindow'          ], DL::TYPE_LONG)
+	GetDC                     = DL::CFunc.new(  User32['GetDC'               ], DL::TYPE_LONG)
+	FillRect		  = DL::CFunc.new(  User32['FillRect'            ], DL::TYPE_LONG)
+	GetClientRect             = DL::CFunc.new(  User32['GetClientRect'       ], DL::TYPE_LONG)
+	GetWindowRect             = DL::CFunc.new(  User32['GetWindowRect'       ], DL::TYPE_LONG)
+	ReleaseDC	          = DL::CFunc.new(  User32['ReleaseDC'           ], DL::TYPE_LONG)
+	SetWindowLong             = DL::CFunc.new(  User32['SetWindowLong'       ], DL::TYPE_LONG)
+	SetWindowPos              = DL::CFunc.new(  User32['SetWindowPos'        ], DL::TYPE_LONG)
+	SystemParametersInfo      = DL::CFunc.new(  User32['SystemParametersInfo'], DL::TYPE_LONG)
+	#---------------------------------------------#
+	# GDI32
+	#---------------------------------------------#
+	CreateSolidBrush  = DL::CFunc.new(  Gdi32['CreateSolidBrush'       ], DL::TYPE_LONG)
+	DeleteObject	  = DL::CFunc.new(  Gdi32['DeleteObject'           ], DL::TYPE_LONG)
+	#---------------------------------------------#
+	# KERNEL32
+	#---------------------------------------------#
+	GetPrivateProfileString   = DL::CFunc.new( Kernel32['GetPrivateProfileString'  ], DL::TYPE_LONG)
+	WritePrivateProfileString = DL::CFunc.new( Kernel32['WritePrivateProfileString'], DL::TYPE_LONG)
+	#---------------------------------------------#
   unless method_defined?(:zeus_fullscreen_update)
-    HWND     = FindWindow.call('RGSS Player', 0)
-    BackHWND = CreateWindowEx.call(0x08000008, 'Static', '', 0x80000000, 0, 0, 0, 0, 0, 0, 0, 0)
-    alias zeus_fullscreen_resize_screen resize_screen
+		HWND     = FindWindow.call([
+				DL::CPtr["RGSS Player"].to_i,
+				0
+		])
+    BackHWND = CreateWindowEx.call([
+				0x08000008,
+				DL::CPtr['Static'].to_i,
+				DL::CPtr[''].to_i,
+				0x80000000,
+			  0,0,0,0,0,0,0,0		
+		])
+		alias zeus_fullscreen_resize_screen resize_screen
     alias zeus_fullscreen_update        update
   end
 private
@@ -46,33 +66,41 @@ private
     @fullscreen_rect ||= screen_rect
     @workarea_rect   ||= workarea_rect
   end
+	
   def borders_size
-    GetWindowRect.call(HWND, wrect = [0, 0, 0, 0].pack('l4'))
-    GetClientRect.call(HWND, crect = [0, 0, 0, 0].pack('l4'))
-    wrect, crect = wrect.unpack('l4'), crect.unpack('l4')
+    GetClientRect.call([HWND,DL::CPtr[wrect = [0, 0, 0, 0].pack('l4')].to_i])
+		GetClientRect.call([HWND,DL::CPtr[crect = [0, 0, 0, 0].pack('l4')].to_i])
+		wrect, crect = wrect.unpack('l4'), crect.unpack('l4')
     Rect.new(0, 0, wrect[2]-wrect[0]-crect[2], wrect[3]-wrect[1]-crect[3])
   end
+	
   def screen_rect
-    Rect.new(0, 0, GetSystemMetrics.call(0), GetSystemMetrics.call(1))
+    Rect.new(0, 0, GetSystemMetrics.call([0]), GetSystemMetrics.call([1]))
   end
+	
   def workarea_rect
-    SystemParametersInfo.call(0x30, 0, rect = [0, 0, 0, 0].pack('l4'), 0)
+    SystemParametersInfo.call([0x30, 0, DL::CPtr[rect = [0, 0, 0, 0].pack('l4')], 0])
     rect = rect.unpack('l4')
     Rect.new(rect[0], rect[1], rect[2]-rect[0], rect[3]-rect[1])
   end
-  def hide_borders() SetWindowLong.call(HWND, -16, 0x14000000) end
-  def show_borders() SetWindowLong.call(HWND, -16, 0x14CA0000) end
-  def hide_back()    ShowWindow.call(BackHWND, 0)              end
-  def show_back
-    ShowWindow.call(BackHWND, 3)
-    UpdateWindow.call(BackHWND)
-    dc    = GetDC.call(BackHWND)
+	
+  def hide_borders() SetWindowLong.call([HWND, -16, 0x14000000]) end
+  def show_borders() SetWindowLong.call([HWND, -16, 0x14CA0000]) end
+  def hide_back()    ShowWindow.call([BackHWND, 0])              end
+  
+	def show_back
+    ShowWindow.call([BackHWND, 3])
+    UpdateWindow.call([BackHWND])
+    dc    = GetDC.call([BackHWND])
     rect  = [0, 0, @fullscreen_rect.width, @fullscreen_rect.height].pack('l4')
-    brush = CreateSolidBrush.call(0)
-    FillRect.call(dc, rect, brush)
-    ReleaseDC.call(BackHWND, dc)
-    DeleteObject.call(brush)
+    brush = CreateSolidBrush.call([0])
+		FillRect.call([dc,DL::CPtr[rect].to_i,brush])
+		ReleaseDC.call([BackHWND, dc])
+    DeleteObject.call([brush])
   end
+#|||||||||||||||||||||||||||||||||||||||||||#	
+#_______Redimenciona_Janela_Game____________#	
+#|||||||||||||||||||||||||||||||||||||||||||#	
   def resize_window(w, h)
     if @fullscreen
       x, y, z = (@fullscreen_rect.width-w)/2, (@fullscreen_rect.height-h)/2, -1
@@ -83,89 +111,91 @@ private
       y = @workarea_rect.y + (@workarea_rect.height - h) / 2
       z = -2
     end
-    SetWindowPos.call(HWND, z, x, y, w, h, 0)
-  end
-  def release_alt
-    inputs = [1,18,2, 1,164,2, 1,165,2].pack('LSx2Lx16'*3)
-    SendInput.call(3, inputs, 28)
+    SetWindowPos.call([HWND, z, x, y, w, h, 0])
   end
 public
+#|||||||||||||||||||||||||||||||||||||||||||#	
+#_______Carregar_Configurações_Modo_Tela____#
+#|||||||||||||||||||||||||||||||||||||||||||#	
   def load_fullscreen_settings
     buffer = [].pack('x256')
     section = 'Fullscreen++'
     filename = './Game.ini'
     get_option = Proc.new do |key, default_value|
-      l = GetPrivateProfileString.call(section, key, default_value, buffer, buffer.size, filename)
-      buffer[0, l]
+      l = GetPrivateProfileString.call([
+					DL::CPtr[section].to_i,
+					DL::CPtr[key].to_i,
+					DL::CPtr[default_value].to_i,
+					DL::CPtr[buffer].to_i,
+					buffer.size,
+					DL::CPtr[filename].to_i
+			])
+			buffer[0, l]
     end
+		#_______Se_nao_haver_configuracao_coloca_a_padrão________________#
     @fullscreen       = get_option.call('Fullscreen'     , '0') == '1'
     @fullscreen_ratio = get_option.call('FullscreenRatio', '0').to_i
     @windowed_ratio   = get_option.call('WindowedRatio'  , '1').to_i
-    toggle_vx_fullscreen if Disable_VX_Fullscreen and vx_fullscreen?
+		#________________________________________________________________#
     fullscreen? ? fullscreen_mode : windowed_mode
   end
+#|||||||||||||||||||||||||||||||||||||||||||#	
+#_______Salvar_Configurações_Modo_Tela______#
+#|||||||||||||||||||||||||||||||||||||||||||#	
   def save_fullscreen_settings
     section = 'Fullscreen++'
     filename = './Game.ini'
     set_option = Proc.new do |key, value|
-      WritePrivateProfileString.call(section, key, value.to_s, filename)
+			WritePrivateProfileString.call([
+				DL::CPtr[section].to_i,
+				DL::CPtr[key].to_i,
+				DL::CPtr[value.to_s].to_i,
+				DL::CPtr[filename].to_i
+			])
     end
+		#____________Salva_Ultima_Configuração_Modo_Tela___________#
     set_option.call('Fullscreen'     , @fullscreen ? '1' : '0')
-    set_option.call('FullscreenRatio', @fullscreen_ratio)
-    set_option.call('WindowedRatio'  , @windowed_ratio)
+    #Evita Erro de Renderização
+		set_option.call('FullscreenRatio', 0)
+		set_option.call('WindowedRatio'  , @windowed_ratio)
+		#__________________________________________________________#
   end
-  def fullscreen?
-    @fullscreen or vx_fullscreen?
-  end
-  def vx_fullscreen?
-    rect = screen_rect
-    #rect.width == 640 and rect.height == 480
-		rect.width == Configs::SCREEN_WIDTH and rect.height == Configs::SCREEN_HEIGHT
-		#Graphics.resize_screen(Configs::SCREEN_WIDTH, Configs::SCREEN_HEIGHT)
-
-  end
+#|||||||||||||||||||||||||||||||||||||||||#	
+#_________Esta_Em_Modo_Tela_Cheia_?_______#
+#|||||||||||||||||||||||||||||||||||||||||#	
+  def fullscreen?;    @fullscreen;  end
+#|||||||||||||||||||||||||||||||||||||||||||||||#	
+#___Altera_Entre_Modo_Tela_Cheia_E_Modo_Janela__#
+#|||||||||||||||||||||||||||||||||||||||||||||||#	
   def toggle_fullscreen
     fullscreen? ? windowed_mode : fullscreen_mode
   end
-  def toggle_vx_fullscreen
-    windowed_mode if @fullscreen and !vx_fullscreen?
-    inputs = [1,18,0, 1,13,0, 1,13,2, 1,18,2].pack('LSx2Lx16'*4)
-    SendInput.call(4, inputs, 28)
-    zeus_fullscreen_update
-    self.ratio += 0 # refresh window size
-  end
-  def vx_fullscreen_mode
-    return if vx_fullscreen?
-    toggle_vx_fullscreen
-  end
+#||||||||||||||||||||||||||||||||||#	
+#_______Modo_Tela_Cheia____________#
+#||||||||||||||||||||||||||||||||||#	
   def fullscreen_mode
-    return if vx_fullscreen?
     initialize_fullscreen_rects
     show_back
     hide_borders
     @fullscreen = true
-    self.ratio += 0 # refresh window size
+    self.ratio += 0
   end
+#||||||||||||||||||||||||||||||||||#	
+#_______Modo_Janela________________#
+#||||||||||||||||||||||||||||||||||#	
   def windowed_mode
-    toggle_vx_fullscreen if vx_fullscreen?
     initialize_fullscreen_rects
     hide_back
     show_borders
     @fullscreen = false
-    self.ratio += 0 # refresh window size
+    self.ratio += 0
   end
-=begin
-  def toggle_ratio
-    return if vx_fullscreen?
-    self.ratio += 1
-  end
-=end
+#||||||||||||||||||||||||||||||||||||||||||||||||||||#	
   def ratio
-    return 1 if vx_fullscreen?
     @fullscreen ? @fullscreen_ratio : @windowed_ratio
   end
+#||||||||||||||||||||||||||||||||||||||||||||||||||||#	
   def ratio=(r)
-    return if vx_fullscreen?
     initialize_fullscreen_rects
     r = 0 if r < 0
     if @fullscreen
@@ -173,8 +203,6 @@ public
       w_max, h_max = @fullscreen_rect.width, @fullscreen_rect.height
     else
       @windowed_ratio = r
-      #w_max = @workarea_rect.width  - @borders_size.width
-      #h_max = @workarea_rect.height - @borders_size.height
 			w_max = Configs::SCREEN_WIDTH - @borders_size.width - 32
 			h_max = Configs::SCREEN_HEIGHT - @borders_size.height - 32
     end
@@ -182,23 +210,28 @@ public
       w, h = w_max, w_max * height / width
       h, w = h_max, h_max * width / height if h > h_max
     else
-      #w, h = width * r, height * r
 			w, h = Configs::SCREEN_WIDTH, Configs::SCREEN_HEIGHT
       return self.ratio = 0 if w > w_max or h > h_max
     end
     resize_window(w, h)
     save_fullscreen_settings
   end
+#|||||||||||||||||||||||||||||||||||||||||||||||||#	
+#_____________Metodo_de_Atualização_______________#
+#|||||||||||||||||||||||||||||||||||||||||||||||||#	
   def update
-    release_alt if Disable_VX_Fullscreen and Input.trigger?(Input::ALT)
     zeus_fullscreen_update
     toggle_fullscreen if Input.trigger?(Input::F5)
-    #toggle_ratio      if Input.trigger?(Input::F6)
   end
+#|||||||||||||||||||||||||||||||||||||||||||||||||#	
+#____________Novo_Redimencionar_Tela______________#
+#|||||||||||||||||||||||||||||||||||||||||||||||||#	
   def resize_screen(width, height)
     zeus_fullscreen_resize_screen(width, height)
     self.ratio += 0 # refresh window size
   end
 end
+#|||||||||||||||||||||||||||||||||||||||||||||||||#	
+#____________Carrega as conf. no Game.ini_________#
+#|||||||||||||||||||||||||||||||||||||||||||||||||#	
 Graphics.load_fullscreen_settings
-Graphics.resize_screen(Configs::SCREEN_WIDTH, Configs::SCREEN_HEIGHT)
